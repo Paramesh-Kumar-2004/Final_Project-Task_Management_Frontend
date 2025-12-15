8c4b3b30d69a0b4e8644fe5a482bd739b4381f57
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-12-15 19:15:06 +0530

    Completed : Create Task Page

--- src/Pages/Task/CreateTasks.jsx
+++ src/Pages/Task/CreateTasks.jsx
@@ -2,6 +2,8 @@ import React, { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Store } from '../../Components/ContextAPI';
 import Sidebar from '../../Components/Sidebar';
+import { API } from '../../API/api';
+import { toast } from 'react-toastify';
 
 
 
@@ -9,38 +11,49 @@ const CreateTasks = () => {
 
     const { task, setTask } = useContext(Store)
 
-    const [data, setData] = useState({
+    const [taskData, setTaskData] = useState({
         title: "",
         description: "",
-        priority: "low"
-    })
-    // category,deadline
+        category: "personal",
+        priority: "low",
+        deadline: ""
+    });
+
     const [tagsInput, setTagsInput] = useState("");
-
-
     const navigate = useNavigate()
+
 
     function HandleChange(e) {
         const { name, value } = e.target;
-        setData({
-            ...data,
+        setTaskData({
+            ...taskData,
             [name]: value
         });
     }
 
-    function HandleSubmit(e) {
+    async function HandleSubmit(e) {
         e.preventDefault();
         try {
-            // navigate("/");
+            const response = await API.post("/task/createtask", taskData)
+            toast(response.data.message, {
+                position: "top-center",
+                autoClose: 2000
+            })
+            navigate("/tasks")
         }
         catch (error) {
-            console.log(error)
+            toast.error(error.response?.data?.message || error.message, {
+                position: "top-center",
+                autoClose: 2000
+            })
         }
         finally {
-            setData({
+            setTaskData({
                 title: "",
                 description: "",
-                type: "low"
+                category: "personal",
+                priority: "low",
+                deadline: ""
             });
         }
 
@@ -50,84 +63,174 @@ const CreateTasks = () => {
         <div className='flex'>
             <Sidebar />
             <div className="flex justify-center items-center min-h-screen bg-[#1B262C] w-full pl-40">
+
                 <form
                     onSubmit={HandleSubmit}
-                    className="bg-[#0d2c41] shadow-lg rounded-xl p-8 w-full max-w-md space-y-6"
+                    className="bg-[#c517d402] border-2 border-gray-700 shadow-sm shadow-sky-400 rounded-xl px-8 py-4 w-full max-w-md space-y-6"
                 >
                     <h2 className="text-2xl font-semibold text-center text-white mb-4">
-                        Create New Task
+                        Create Task
                     </h2>
 
+                    {/* Title */}
                     <div>
                         <label
                             htmlFor="title"
-                            className="block text-white font-medium mb-2"
+                            className="block text-white font-medium text-xl mb-2"
                         >
                             Title
                         </label>
                         <input
-                            type="text"
-                            id="title"
                             name='title'
-                            onChange={HandleChange}
-                            value={data.title}
-                            placeholder="Enter Your Title"
+                            value={taskData.title}
+                            onChange={(e) => HandleChange(e)}
+                            placeholder="Write Your Task..."
                             required
-                            className="w-full border text-sky-50 border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-400 transition-all duration-300"
+                            className="w-full p-2 rounded-md text-white outline-none border-2 border-sky-500 resize-none"
                         />
                     </div>
 
+                    {/* Description */}
                     <div>
                         <label
-                            htmlFor="notes"
-                            className="block text-white font-medium mb-2"
+                            htmlFor="description"
+                            className="block text-white font-medium text-xl mb-2"
                         >
                             Description
                         </label>
-                        <textarea
-                            id="notes"
-                            placeholder="Write Your Notes Here..."
+                        <input
                             name='description'
-                            onChange={HandleChange}
-                            value={data.description}
+                            value={taskData.description}
+                            onChange={(e) => HandleChange(e)}
+                            placeholder="Write Your Description..."
                             required
-                            rows="4"
-                            className="w-full border text-sky-50 border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-400 transition-all duration-300"
-                        ></textarea>
+                            className="w-full p-2 rounded-md text-white outline-none border-2 border-sky-500 resize-none"
+                        />
                     </div>
 
-                    <div>
+                    {/* Category & Priority */}
+                    <div className='flex flex-wrap justify-between gap-3'>
+
+                        {/* Category - ["work", "personal", "prjects"] */}
+                        <div className='flex-1'>
+                            <label
+                                htmlFor="category"
+                                className="block text-white font-medium text-xl mb-2"
+                            >
+                                Category
+                            </label>
+                            <select
+                                name="category"
+                                value={taskData.category}
+                                onChange={(e) => HandleChange(e)}
+                                className="w-full p-2 rounded-md border-2 border-sky-500 text-white text-lg outline-none cursor-pointer"
+                            >
+                                <option value="work" className="text-lg bg-[#1B262C]">Work</option>
+                                <option value="personal" className="text-lg bg-[#1B262C]">Personal</option>
+                                <option value="prjects" className="text-lg bg-[#1B262C]">Prjects</option>
+                            </select>
+                        </div>
+
+                        {/* Priority - ["low", "medium", "high"] */}
+                        <div className='flex-1'>
+                            <label
+                                htmlFor="priority"
+                                className="block text-white font-medium text-xl mb-2"
+                            >
+                                Priority
+                            </label>
+                            <select
+                                name="priority"
+                                value={taskData.priority}
+                                onChange={(e) => HandleChange(e)}
+                                className="w-full p-2 rounded-md border-2 border-sky-500 text-white text-lg outline-none cursor-pointer"
+                            >
+                                <option value="low" className="text-lg bg-[#1B262C]">Low</option>
+                                <option value="medium" className="text-lg bg-[#1B262C]">Medium</option>
+                                <option value="high" className="text-lg bg-[#1B262C]">High</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    {/* Status - ["pending", "in-progress", "completed"] */}
+                    {/* <div>
                         <label
-                            htmlFor="notes"
-                            className="block text-white font-medium mb-2"
+                            htmlFor="title"
+                            className="block text-white font-medium text-xl mb-2"
+                        >
+                            Status
+                        </label>
+                        <select
+                            name="status"
+                            value={taskData.status}
+                            onChange={(e) => HandleChange(e)}
+                            className="w-full p-2 rounded-md border-2 border-sky-500 text-white text-lg outline-none cursor-pointer"
+                        >
+                            <option value="pending" className="text-lg bg-[#1B262C]">Pending</option>
+                            <option value="in-progress" className="text-lg bg-[#1B262C]">In-Progress</option>
+                            <option value="completed" className="text-lg bg-[#1B262C]">Completed</option>
+                        </select>
+                    </div> */}
+
+                    {/* Priority - ["low", "medium", "high"] */}
+                    {/* <div>
+                        <label
+                            htmlFor="priority"
+                            className="block text-white font-medium text-xl mb-2"
                         >
                             Priority
                         </label>
                         <select
                             name="priority"
-                            className="w-full border text-sky-50 border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-400 transition-all duration-300 bg-[#0d2c41]"
+                            value={taskData.priority}
+                            onChange={(e) => HandleChange(e)}
+                            className="w-full p-2 rounded-md border-2 border-sky-500 text-white text-lg outline-none cursor-pointer"
                         >
-                            <option value="low">Low</option>
-                            <option value="medium">Medium</option>
-                            <option value="high">High</option>
+                            <option value="low" className="text-lg bg-[#1B262C]">Low</option>
+                            <option value="medium" className="text-lg bg-[#1B262C]">Medium</option>
+                            <option value="high" className="text-lg bg-[#1B262C]">High</option>
                         </select>
+                    </div> */}
+
+                    {/* Deadline - Date */}
+                    <div>
+                        <label
+                            htmlFor="deadline"
+                            className="block text-white font-medium text-xl mb-2"
+                        >
+                            Deadline
+                        </label>
+                        <input
+                            type="date"
+                            name="deadline"
+                            value={
+                                taskData.deadline
+                                    ? new Date(taskData.deadline).toISOString().split("T")[0]
+                                    : ""
+                            }
+                            onChange={(e) => HandleChange(e)}
+                            required
+                            className="w-full p-2 rounded-md border-2 border-sky-500 text-white text-lg outline-none"
+                        />
                     </div>
 
-                    <div className='flex gap-6'>
+                    <div className="flex gap-6">
                         <button
                             type="submit"
-                            className="w-full bg-blue-500 text-white font-semibold py-2 rounded-md hover:bg-blue-600 transition-all duration-300 cursor-pointer"
+                            className="w-full bg-green-600 text-white font-semibold py-2 rounded-md hover:bg-green-700 transition-all duration-300 cursor-pointer"
                         >
                             Create Task
                         </button>
 
                         <button
+                            type="button"
                             className="w-full bg-red-600 text-white font-semibold py-2 rounded-md hover:bg-red-700 transition-all duration-300 cursor-pointer"
-                            onClick={() => navigate("/")}
+                            onClick={() => navigate(-1)}
                         >
                             Cancel
                         </button>
                     </div>
+
                 </form>
             </div>
         </div>
