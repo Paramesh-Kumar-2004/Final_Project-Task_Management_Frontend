c7cb230bd7567aa98b1cc812ad04b24e0e46c06a
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-12-20 22:05:58 +0530

    Working On Styles In PieChart

--- src/Components/Charts/PieChart.jsx
+++ src/Components/Charts/PieChart.jsx
@@ -36,7 +36,9 @@ const PieChart = ({ total, completed }) => {
         <div className="bg-[#1B262C] rounded-lg h-full">
             <div className="p-4 rounded-lg h-[400px] ">
                 <h2 className="text-center text-lg font-semibold mb-3">Task Completion</h2>
-                <Pie data={data} options={options} />
+                <div className="h-10/12">
+                    <Pie data={data} options={options} />
+                </div>
                 <p className="text-center mt-3 font-medium">
                     Completion Rate: {total ? Math.round((completed / total) * 100) : 0}%
                 </p>
