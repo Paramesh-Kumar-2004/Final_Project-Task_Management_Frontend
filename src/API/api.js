59ec1e592c3a8098b94bfb6736f940e9ff0f76f2
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2026-02-13 22:27:25 +0530

    Adding API Iterceptors

--- src/API/api.js
+++ src/API/api.js
@@ -7,7 +7,7 @@ const baseURL = import.meta.env.DEV
     : "https://final-project-task-management-backend-1.onrender.com/api/v1";
 
 
-export const API = axios.create({
+const API = axios.create({
     baseURL,
     // "http://localhost:2004/api/v1",
     // baseURL: "https://final-project-task-management-backend-1.onrender.com/api/v1",
@@ -15,12 +15,34 @@ export const API = axios.create({
 });
 
 
-API.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
-
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
+// 1. Request Interceptor
+API.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
+);
 
-    return config;
-});
+
+// 2. Response Interceptor
+API.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response && error.response.status === 401) {
+            localStorage.removeItem('token');
+            // window.location.href = '/login';
+
+            const base = window.location.pathname;
+            window.location.href = `${base}#/login`;
+        }
+        return Promise.reject(error);
+    }
+);
+
+export { API }
